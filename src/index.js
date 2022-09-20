f7e1a7d932b667a7d63db62ac847eaecd44a7e10
VladyslavIT <vlad.chernyy.1997@gmail.com>
2022-09-20 18:31:16 +0300

    x

--- src/index.js
+++ src/index.js
@@ -1,10 +1,29 @@
-import React from 'react';
+// import React, { Children } from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from 'components/App';
 import './index.css';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+// ReactDOM.createRoot(document.getElementById('root')).render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
+
+
+const imageUrl =
+  "https://images.pexels.com/photos/461198/pexels-photo-461198.jpeg?dpr=2&h=480&w=640";
+const productPrice = 10.99;
+
+const product = (
+  <div>
+    <img src={imageUrl} alt="Tacos With Lime" width="640" />
+    <h2>Tacos With Lime</h2>
+    <p>Price: {productPrice}$</p>
+    <button type="button">Add to cart</button>
+  </div>
 );
+
+function Painting(props) {
+  return product;
+}
+ReactDOM.createRoot(document.getElementById("root")).render(<Painting/>);
